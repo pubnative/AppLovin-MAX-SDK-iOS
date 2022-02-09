2791e905313689e5180dc8bcb3a27388bff30015
Eros <eros902002@googlemail.com>
2022-02-09 17:30:20 +0100

    [OMI-1133] Finish native adapter. Signal data still missing

--- AppLovin MAX Demo App - ObjC/AppLovin MAX Demo App - ObjC/MAX/VerveAdapters/ALVerveMediationAdapter.m
+++ AppLovin MAX Demo App - ObjC/AppLovin MAX Demo App - ObjC/MAX/VerveAdapters/ALVerveMediationAdapter.m
@@ -30,6 +30,18 @@
 - (instancetype)initWithParentAdapter:(ALVerveMediationAdapter *)parentAdapter andNotify:(id<MAAdViewAdapterDelegate>)delegate;
 @end
 
+@interface ALVerveMediationAdapterNativeAdDelegate : NSObject<HyBidNativeAdLoaderDelegate, HyBidNativeAdFetchDelegate, HyBidNativeAdDelegate>
+@property (nonatomic, weak) ALVerveMediationAdapter *parentAdapter;
+@property (nonatomic, strong) id<MANativeAdAdapterDelegate> delegate;
+@property (nonatomic, strong) NSDictionary<NSString *, id> *serverParameters;
+- (instancetype)initWithParentAdapter:(ALVerveMediationAdapter *)parentAdapter serverParameters:(NSDictionary<NSString *, id> *)serverParameters andNotify:(id<MANativeAdAdapterDelegate>)delegate;
+@end
+
+@interface MAVerveNativeAd : MANativeAd
+@property (nonatomic, weak) ALVerveMediationAdapter *parentAdapter;
+- (instancetype)initWithParentAdapter:(ALVerveMediationAdapter *)parentAdapter builderBlock:(NS_NOESCAPE MANativeAdBuilderBlock)builderBlock;
+@end
+
 @interface ALVerveMediationAdapter()
 
 // Interstitial
@@ -43,6 +55,10 @@
 // AdView
 @property (nonatomic, strong) HyBidAdView *adViewAd;
 @property (nonatomic, strong) ALVerveMediationAdapterAdViewDelegate *adViewAdapterDelegate;
+
+// NativeAd
+@property (nonatomic, strong) HyBidNativeAd *nativeAd;
+@property (nonatomic, strong) ALVerveMediationAdapterNativeAdDelegate *nativeAdAdapterDelegate;
 
 @end
 
@@ -118,6 +134,12 @@ static MAAdapterInitializationStatus ALVerveInitializationStatus = NSIntegerMin;
     self.adViewAd = nil;
     
     self.adViewAdapterDelegate = nil;
+    
+    if (self.nativeAd) {
+        [self.nativeAd stopTracking];
+    }
+    self.nativeAd = nil;
+    self.nativeAdAdapterDelegate = nil;
 }
 
 #pragma mark - MASignalProvider Methods
@@ -215,6 +237,10 @@ static MAAdapterInitializationStatus ALVerveInitializationStatus = NSIntegerMin;
     
     [self updateConsentWithParameters: parameters];
     [self updateMuteStateForParameters: parameters];
+    
+    HyBidNativeAdLoader *nativeAdLoader = [[HyBidNativeAdLoader alloc] init];
+    self.nativeAdAdapterDelegate = [[ALVerveMediationAdapterNativeAdDelegate alloc] initWithParentAdapter: self serverParameters: parameters.serverParameters andNotify: delegate];
+    [nativeAdLoader loadNativeAdWithDelegate:self.nativeAdAdapterDelegate withZoneID:@"7"];
 }
 
 #pragma mark - Shared Methods
@@ -490,6 +516,163 @@ static MAAdapterInitializationStatus ALVerveInitializationStatus = NSIntegerMin;
 {
     [self.parentAdapter log: @"AdView clicked: %@", adView];
     [self.delegate didClickAdViewAd];
+}
+
+@end
+
+@implementation ALVerveMediationAdapterNativeAdDelegate
+
+- (instancetype)initWithParentAdapter:(ALVerveMediationAdapter *)parentAdapter serverParameters:(NSDictionary<NSString *,id> *)serverParameters andNotify:(id<MANativeAdAdapterDelegate>)delegate
+{
+    self = [super init];
+    if ( self )
+    {
+        self.parentAdapter = parentAdapter;
+        self.serverParameters = serverParameters;
+        self.delegate = delegate;
+    }
+    return self;
+}
+
+- (void)nativeLoaderDidLoadWithNativeAd:(HyBidNativeAd *)nativeAd
+{
+    [self.parentAdapter log: @"Native ad loaded"];
+    
+    if ( !self.parentAdapter.nativeAd )
+    {
+        [self.parentAdapter log: @"Native ad failed to load: no fill"];
+        [self.delegate didFailToLoadNativeAdWithError: MAAdapterError.noFill];
+            
+        return;
+    }
+    
+    self.parentAdapter.nativeAd = nativeAd;
+    
+    NSString *templateName = [self.serverParameters al_stringForKey: @"template" defaultValue: @""];
+    BOOL isTemplateAd = [templateName al_isValidString];
+    
+    if ( ![self hasRequiredAssetsInAd: self.parentAdapter.nativeAd isTemplateAd: isTemplateAd] )
+    {
+        [self.parentAdapter e: @"Native ad (%@) does not have required assets.", nativeAd];
+        [self.delegate didFailToLoadNativeAdWithError: [MAAdapterError errorWithCode: -5400 errorString: @"Missing Native Ad Assets"]];
+            
+        return;
+    }
+    
+    [nativeAd fetchNativeAdAssetsWithDelegate:self];
+}
+
+- (void)nativeLoaderDidFailWithError:(NSError *)error
+{
+    [self.parentAdapter log: @"Native ad failed to load: %@", error];
+    
+    MAAdapterError *adapterError = [ALVerveMediationAdapter toMaxError: error];
+    [self.delegate didFailToLoadNativeAdWithError: adapterError];
+}
+
+- (void)nativeAdDidFinishFetching:(HyBidNativeAd *)nativeAd
+{
+    
+}
+
+- (void)nativeAd:(HyBidNativeAd *)nativeAd didFailFetchingWithError:(NSError *)error
+{
+    [self.parentAdapter log: @"Native ad failed to fetch assets: %@", error];
+    
+    MAAdapterError *adapterError = [ALVerveMediationAdapter toMaxError: error];
+    [self.delegate didFailToLoadNativeAdWithError: adapterError];
+}
+
+- (void)nativeAd:(HyBidNativeAd *)nativeAd impressionConfirmedWithView:(UIView *)view
+{
+    [self.parentAdapter log: @"Native ad shown"];
+    if (self.delegate) {
+        [self.delegate didDisplayNativeAdWithExtraInfo:nil];
+    }
+}
+
+- (void)nativeAdDidClick:(HyBidNativeAd *)nativeAd
+{
+    [self.parentAdapter log: @"Native ad clicked"];
+    if (self.delegate) {
+        [self.delegate didClickNativeAd];
+    }
+}
+
+- (BOOL)hasRequiredAssetsInAd:(HyBidNativeAd *)nativeAd isTemplateAd:(BOOL)isTemplateAd
+{
+    if ( isTemplateAd )
+    {
+        return [nativeAd.title al_isValidString];
+    }
+    else
+    {
+        return [nativeAd.title al_isValidString]
+        && [nativeAd.callToActionTitle al_isValidString]
+        && [nativeAd.bannerUrl al_isValidString];
+    }
+}
+
+- (void)processNativeAd
+{
+    dispatchOnMainQueueNow(^{
+        MAVerveNativeAd *verveNativeAd = [[MAVerveNativeAd alloc] initWithParentAdapter:self.parentAdapter builderBlock:^(MANativeAdBuilder *builder){
+            
+            UIView* bannerView = self.parentAdapter.nativeAd.banner;
+            if (bannerView) {
+                builder.mediaView = bannerView;
+            }
+            
+            UIImage *iconImage = self.parentAdapter.nativeAd.icon;
+            if (iconImage) {
+                MANativeAdImage* icon = [[MANativeAdImage alloc] initWithImage: iconImage];
+                builder.icon = icon;
+            }
+            
+            if ([self.parentAdapter.nativeAd.title al_isValidString]) {
+                builder.title = self.parentAdapter.nativeAd.title;
+            }
+            
+            if ([self.parentAdapter.nativeAd.body al_isValidString]) {
+                builder.body = self.parentAdapter.nativeAd.body;
+            }
+            
+            if ([self.parentAdapter.nativeAd.callToActionTitle al_isValidString]) {
+                builder.callToAction = self.parentAdapter.nativeAd.callToActionTitle;
+            }
+            
+            HyBidContentInfoView *contentInfoView = self.parentAdapter.nativeAd.contentInfo;
+            if (contentInfoView) {
+                builder.optionsView = contentInfoView;
+            }
+        }];
+        
+        [self.delegate didLoadAdForNativeAd:verveNativeAd withExtraInfo:nil];
+    });
+}
+
+@end
+
+@implementation MAVerveNativeAd
+
+- (instancetype)initWithParentAdapter:(ALVerveMediationAdapter *)parentAdapter builderBlock:(NS_NOESCAPE MANativeAdBuilderBlock)builderBlock
+{
+    self = [super initWithFormat: MAAdFormat.native builderBlock: builderBlock];
+    if ( self )
+    {
+        self.parentAdapter = parentAdapter;
+    }
+    return self;
+}
+
+- (void)prepareViewForInteraction:(MANativeAdView *)nativeAdView
+{
+    if (!self.parentAdapter.nativeAd) {
+        [self.parentAdapter e: @"Failed to register native ad views: native ad is nil."];
+        return;
+    }
+    
+    [self.parentAdapter.nativeAd startTrackingView:nativeAdView withDelegate:self.parentAdapter.nativeAdAdapterDelegate];
 }
 
 @end
